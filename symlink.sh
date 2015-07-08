ae48b058a1201b8b999a88b3115433d0fc3748e3
Antoni <contact@antoni.systems>
2015-07-08 15:12:39 +0200

    Added working symlink script

--- symlink.sh
+++ symlink.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-DOTFILES_DIR='~/dotfiles/'
+DOTFILES_DIR=~/dotfiles
+# DOTFILES_DIR='/home/antoni/dotfiles'
+
+# Version of clang-format, should be taken from /usr/bin/clang-format-X.Y
 CLANG_FORMAT_VERSION=3.5
-DOTFILES=(vimrc tmux.conf gitconfig gitignore)
+DOTFILES=(vimrc tmux.conf gitconfig gitignore ghci gvimrc hgrc)
+
+# Xrdb merge
+xrdb $DOTFILES_DIR/Xresources.solarized
 
 # Symlink the files in the current directory with corresponding dotfiles in
 # the home directory
@@ -12,28 +18,25 @@ do
     ln -s ~/dotfiles/$f ~/.$f > /dev/null
 done;
 
-# TODO: Terminator
-# TODO: Symlink .gitignore
-ln -s $DOTFILES_DIR+gitignore ~/.gitignore
-# TODO: Symlink .gitconfig
-ln -s gitconfig ~/.gitconfig
-# TODO: Symlink sshconfig to .ssh/config directory
-# TODO: Symlink scripts/ directory
-ln -s /home/antoni/dotfiles/eclipse.ini eclipse.ini
+# Terminator
+ln -fs $DOTFILES_DIR/terminator/config ~/.config/terminator/config
 
-# .ghci file
-# TODO: Symlink
+# SSH config
+ln -fs $DOTFILES_DIR/sshconfig ~/.ssh/config
+
+# ~/scripts directory
+ln -fs $DOTFILES_DIR/scripts ~/scripts
+
+# .ghci access
 chmod g-w ~/.ghci
 
-# TODO: .gvimrc, .gitconfig, .hgrc
-
 # /usr/bin symlinks
-sudo ln -s /usr/bin/google-chrome-stable /usr/bin/g
-
+# Chrome
+sudo ln -fs /usr/bin/google-chrome-stable /usr/bin/g
 # clang-format
-sudo ln -r -s /usr/bin/clang-format-$CLANG_FORMAT_VERSION /usr/bin/clang-format
+sudo ln -fs /usr/bin/clang-format-$CLANG_FORMAT_VERSION /usr/bin/clang-format
 
 # CUDA snippets for Vim
-wget 
-https://gist.githubusercontent.com/antoni/d8ac9973b2f28765b329/raw/811fa82e6ff738e06c11453bfa93d846d76d2386/cuda.snippets 
-&& mv cuda.snippets ~/.vim/bundle/vim-snippets/snippets/
+# wget 
+# https://gist.githubusercontent.com/antoni/d8ac9973b2f28765b329/raw/811fa82e6ff738e06c11453bfa93d846d76d2386/cuda.snippets 
+# && mv cuda.snippets ~/.vim/bundle/vim-snippets/snippets/
