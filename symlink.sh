d8549595f966c3e4c84b91206490ef4c48b9b9ac
Antoni Rościszewski <antoni.rosciszewski@gmail.com>
2015-09-16 17:56:20 +0200

    Updated symlink for Clang. Removed few TODOs from .vimrc

--- symlink.sh
+++ symlink.sh
@@ -4,9 +4,11 @@ DOTFILES_DIR=~/dotfiles
 
 # Version of clang-format, should be taken from /usr/bin/clang-format-X.Y,
 # same for clang-modernize
+# CLANG_VERSION=`\ls /usr/bin/clang-?.? -1 | cut -d'-' -f2 | sort -gr | head -1`
 CLANG_VERSION=3.5
-CLANG_FORMAT_VERSION=3.5
-CLANG_MODERNIZE_VERSION=3.5
+echo "Clang version symlinked:" $CLANG_VERSION
+CLANG_FORMAT_VERSION=$CLANG_VERSION
+CLANG_MODERNIZE_VERSION=$CLANG_VERSION
 
 DOTFILES=(vimrc tmux.conf gitconfig gitignore ghci gvimrc hgrc)
 
@@ -25,9 +27,8 @@ done;
 ln -fs ${DOTFILES_DIR}/terminator/config ~/.config/terminator/config
 # i3-wm
 I3WM_DIR=~/.config/i3/
-if ! [[ -f $I3WM_DIR ]]; then mkdir -p $I3WM_DIR fi;
+if ! [[ -f $I3WM_DIR ]]; then mkdir -p $I3WM_DIR; fi;
 ln -fs ${DOTFILES_DIR}/i3.config ~/.config/i3/config
-
 
 # SSH config
 ln -fs ${DOTFILES_DIR}/sshconfig ~/.ssh/config
@@ -42,6 +43,7 @@ chmod g-w ~/.ghci
 # Chrome
 sudo ln -fs /usr/bin/google-chrome-stable /usr/bin/g
 # clang
+sudo ln -fs /usr/bin/clang-$CLANG_VERSION /usr/bin/clang
 sudo ln -fs /usr/bin/clang++-$CLANG_VERSION /usr/bin/clang++
 # clang-format
 sudo ln -fs /usr/bin/clang-format-$CLANG_FORMAT_VERSION /usr/bin/clang-format
