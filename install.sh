31c10539ca50a08dbe39da4f764d5cc55717f6ef
Antoni <contact@antoni.systems>
2016-07-07 12:57:00 +0200

    Added install.sh. Modified aliases and symlinks

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install required packages
+PACKAGES=(slock xbindkeys haskell clang)
+
+if [ -f /etc/debian_version ]; then
+    echo "Installing required packages on Debian"
+    apt-get install -y $PACKAGES
+    echo "This is debian based distro"
+elif [ -f /etc/redhat-release ]; then
+    echo "Installing required packages on Fedora"
+    dnf install -y $PACKAGES
+else
+    echo "TODO: Mac OS X install"
+fi
