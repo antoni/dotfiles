a131cbf8dd63a65c75d3e0c5755495f598c9b07d
Antoni Rościszewski <antoni.rosciszewski@gmail.com>
2017-08-30 22:58:14 +0200

    Add Atom config

--- symlink.sh
+++ symlink.sh
@@ -202,3 +202,9 @@ function install_airbnb_eslint() {
 # install_npm_packages
 # install_airbnb_eslint
 
+# Atom
+
+for atom in `\ls atom`; do
+    rm -f ~/.atom/$atom;
+    ln -fs ~/dotfiles/atom/$atom $HOME/.atom/$atom;
+done
