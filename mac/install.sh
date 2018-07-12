61c4f358b3c43a818177722f0c807671d08aada5
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2018-07-12 12:21:25 +0200

    Few minor changes, mostly in macOS configuration

--- mac/install.sh
+++ mac/install.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 
+function command_exists () { type "$1" &> /dev/null ; }
+
 # Returns internal application ID for given application name
-function app_id { osascript -e "id of app \"$1\"" }
+function app_id {
+    osascript -e "id of app \"$1\""
+}
 
 # Change default application for given file type
-
-# TODO: Check if duti is available
-
-duti -s `app_id "PYM Player"` .mp4 all
-duti -s `app_id "Google Chrome"` .html all
-duti -s `app_id "TextMate"` .txt all
+if command_exists duti; then
+    duti -s `app_id "PYM Player"` .avi all;
+    duti -s `app_id "PYM Player"` .mkv all;
+    duti -s `app_id "PYM Player"` .mp4 all;
+    duti -s `app_id "TextMate"`   .html all;
+    duti -s `app_id "TextMate"`   .txt all;
+else
+    printf "You have to install 'duti' first"
+fi
 
 # Symlink TextMate
-sudo ln -s /Applications/TextMate.app/Contents/Resources/mate /usr/local/bin/mate
+sudo ln -fs /Applications/TextMate.app/Contents/Resources/mate /usr/local/bin/mate
 
 # Clear the Dock
 dockutil --remove all
