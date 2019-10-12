b27dea9e7e7401d28894bf00509bf6ecd124ae7d
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2019-10-12 10:53:48 +0530

    Add more installation scripts for macOS

---
+++ mac/sleep.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# TODO: Add this to services:  /usr/local/sbin/sleepwatcher --verbose --sleep ~/.sleep --wake ~/.wake
+source ~/dotfiles/mac/sleep_and_wake.sh && sleep
---
+++ mac/sleep_watcher/setup_sleep_watcher.sh
@@ -0,0 +1,6 @@
+ln -sfv ~/dotfiles/mac/sleep_watcher/de.bernhard-baehr.sleepwatcher-20compatibility-localuser.plist ~/Library/LaunchAgents/
+
+# Unload in case it has been loaded with incorrect configuration before
+launchctl unload ~/Library/LaunchAgents/de.bernhard-baehr.sleepwatcher-20compatibility-localuser.plist &> /dev/null
+launchctl load ~/Library/LaunchAgents/de.bernhard-baehr.sleepwatcher-20compatibility-localuser.plist
+
