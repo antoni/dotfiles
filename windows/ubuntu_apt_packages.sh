444d9a6d3193eaff4ff18363806955fc9d26cdcc
Antoni <contact@antoni.systems>
2022-05-25 19:52:33 +0200

    WIP Wed May 25 19:52:33 CEST 2022

--- windows/ubuntu_apt_packages.sh
+++ windows/ubuntu_apt_packages.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env bash
 
 APT_PACKAGES=(apache-httpd
-vim
-tmux
-nodejs
-golang
-npm
-docker
-htop
-nginx
-jq
-gitk
-poppler-utils
-mc
-exiftool
-rand
-subversion
-tig
-zip
-git-lfs
-librecad
-p7zip
-sudo apt-get install libasound2
-docker-compose
-sudo apt-get install default-jdk scala
-cmake
-sudo apt-get install -y shellcheck
+	vim
+	tmux
+	nodejs
+	golang
+	npm
+	docker
+	htop
+	nginx
+	jq
+	gitk
+	poppler-utils
+	mc
+	exiftool
+	rand
+	subversion
+	tig
+	zip
+	git-lfs
+	librecad
+	p7zip
+	sudo apt-get install libasound2
+	docker-compose
+	sudo apt-get install default-jdk scala
+	cmake
+	sudo apt-get install -y shellcheck
 )
