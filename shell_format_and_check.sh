186f7bf8ec60a565ba9ae4e18a0d5bb8c198bfd7
Antoni <contact@antoni.systems>
2022-06-12 22:28:40 +0200

    WIP Wed Jun  1 11:25:53 CEST 2022

--- shell_format_and_check.sh
+++ shell_format_and_check.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-# Prepend text at the beginning of each line
-function prepend() { while read line; do echo "${1}${line}"; done; }
+source "$HOME"/dotfiles/utils.sh
 
 # Files that are not recognized as shell by shfmt
-shell_files="paths
+unrecognized_shell_files="paths
 aliases
 profile
 bashrc
@@ -17,13 +16,4 @@ fzf.sh
 shell_format_and_check.sh
 %s" # files found automatically using shfmt
 
-ALL_FILES=$(printf "$shell_files" "$(shfmt -f .)" | sort -u)
-
-# Format code
-printf "%s" "$ALL_FILES" | xargs shfmt -l -w || exit 1
-
-# Lint code
-printf "%s" "$ALL_FILES" | xargs shellcheck --format diff | git apply --allow-empty || exit 1
-
-# Run again for files that could not be autofixed
-printf "%s" "$ALL_FILES" | xargs shellcheck || exit 1
+shell_check_and_format "$unrecognized_shell_files"
