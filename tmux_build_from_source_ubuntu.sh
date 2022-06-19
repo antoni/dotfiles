506a2ebd2fb810913e6be85fedf19f33599f346a
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2022-06-19 14:37:30 +0200

    WIP Sun Jun 19 14:37:30 CEST 2022

---
+++ tmux_build_from_source_ubuntu.sh
@@ -0,0 +1,18 @@
+# Steps to build and install tmux from source on Ubuntu.
+# Takes < 25 seconds on EC2 env [even on a low-end config instance].
+VERSION="3.3a"
+sudo apt-get -y remove tmux
+sudo apt-get -y install wget tar libevent-dev libncurses-dev
+wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
+tar xf tmux-${VERSION}.tar.gz
+rm -f tmux-${VERSION}.tar.gz
+cd tmux-${VERSION}
+./configure
+make
+sudo make install
+cd -
+sudo rm -rf /usr/local/src/tmux-*
+sudo mv tmux-${VERSION} /usr/local/src
+
+## Logout and login to the shell again and run.
+## tmux -V
