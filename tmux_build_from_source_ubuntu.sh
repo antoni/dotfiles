a548bd1624137620a421c6deaf6a273a7081ad1f
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2022-06-22 11:47:02 +0200

    WIP Wed Jun 22 11:47:02 CEST 2022

--- tmux_build_from_source_ubuntu.sh
+++ tmux_build_from_source_ubuntu.sh
@@ -6,11 +6,11 @@ sudo apt-get -y install wget tar libevent-dev libncurses-dev
 wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
 tar xf tmux-${VERSION}.tar.gz
 rm -f tmux-${VERSION}.tar.gz
-cd tmux-${VERSION}
+cd tmux-${VERSION} || exit
 ./configure
 make
 sudo make install
-cd -
+cd - || exit
 sudo rm -rf /usr/local/src/tmux-*
 sudo mv tmux-${VERSION} /usr/local/src
 
