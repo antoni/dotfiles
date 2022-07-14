129300d81c5784b69698a808b90cdde19871303a
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2022-07-14 17:17:46 +0200

    WIP Thu Jul 14 17:17:46 CEST 2022

--- windows/ubuntu_apt_packages.sh
+++ windows/ubuntu_apt_packages.sh
@@ -26,6 +26,6 @@ APT_PACKAGES=(apache-httpd
 	sudo apt-get install default-jdk scala
 	cmake
 	sudo apt-get install -y shellcheck
-sudo apt-get install build-essential cmake pkg-config libicu-dev zlib1g-dev libcurl4-openssl-dev libssl-dev ruby-dev
-sudo apt-get install ruby-full build-essential zlib1g-dev
+	sudo apt-get install build-essential cmake pkg-config libicu-dev zlib1g-dev libcurl4-openssl-dev libssl-dev ruby-dev
+	sudo apt-get install ruby-full build-essential zlib1g-dev
 )
