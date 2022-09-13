9b44de7b62b0634b1485931d1ec35fdcbe56b3d7
Antoni <contact@antoni.systems>
2022-09-13 13:39:32 +0200

    WIP Tue 13 Sep 2022 01:39:32 PM CEST

---
+++ image_utils.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+function collage() {
+  montage -density 300 -tile 2x0 -geometry +5+50 -border 10 ./*.png collage.png
+}
+
+function pixelate() {
+  local filename="$1"
+  name=$(filename_without_extension "$filename")
+  local name
+
+  extension=$(filename_extension "$filename")
+  local extension
+
+  convert -scale 10% -scale 1000% "$filename" "$name""_pixelated""$extension"
+}
+
+function remove_exif_data() {
+  local filename=$1
+  exiftool -EXIF= "$filename"
+}
