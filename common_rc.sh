d212812cfbf39fc1228c13e9f50c4371098f3c28
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2022-09-18 17:02:16 +0200

    WIP Sun Sep 18 17:02:16 CEST 2022

--- common_rc.sh
+++ common_rc.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 function export_variables() {
-  export DEFAULT_CITY="Warsaw"
-  export DEFAULT_COORDS="52.23:21.012'"
-  export GITHUB_USER="antoni"
-  # TODO: Get username dynamically: powershell.exe '$env:UserName'
-  # (it takes time, return value should be stored somewhere)
-  export WINDOWS_USERNAME="Komputer"
-  export HOMEBREW_NO_AUTO_UPDATE=1
-  export BASH=/usr/local/bin/bash
+	export DEFAULT_CITY="Warsaw"
+	export DEFAULT_COORDS="52.23:21.012'"
+	export GITHUB_USER="antoni"
+	# TODO: Get username dynamically: powershell.exe '$env:UserName'
+	# (it takes time, return value should be stored somewhere)
+	export WINDOWS_USERNAME="Komputer"
+	export HOMEBREW_NO_AUTO_UPDATE=1
+	export BASH=/usr/local/bin/bash
 }
 export_variables
 
