7f82aac8e91dec744b42b9b7f17f2285e0c4757c
Antoni <contact@antoni.systems>
2022-09-20 17:19:06 +0200

    WIP Tue Sep 20 17:19:06 CEST 2022

---
+++ mac/transmission.sh
@@ -0,0 +1,11 @@
+defaults write org.m0k.transmission DeleteOriginalTorrent -bool true
+defaults write org.m0k.transmission DownloadAsk -bool false
+defaults write org.m0k.transmission MagnetOpenAsk -bool false
+defaults write org.m0k.transmission CheckRemoveDownloading -bool true
+defaults write org.m0k.transmission DownloadChoice -string 'Constant'
+defaults write org.m0k.transmission DownloadFolder -string '$HOME/Documents'
+defaults write org.m0k.transmission UseIncompleteDownloadFolder -bool true
+defaults write org.m0k.transmission IncompleteDownloadFolder -string '$HOME/Documents'
+defaults write org.m0k.transmission WarningDonate -bool false
+defaults write org.m0k.transmission WarningLegal -bool false
+defaults write org.m0k.transmission RandomPort -bool true
