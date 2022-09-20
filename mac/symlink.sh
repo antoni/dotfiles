c7e230e3b83ec434e55c7cc09b422ffbc962e371
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2022-09-20 17:17:59 +0200

    WIP Tue Sep 20 17:17:59 CEST 2022

--- mac/symlink.sh
+++ mac/symlink.sh
@@ -7,4 +7,4 @@ MAC_DIR=$DOTFILES_DIR/mac
 ln -fs "$MAC_DIR"/karabiner ~/.config/
 
 # Transmission config
-ln -fs "$MAC_DIR"/org.m0k.transmission.plist "$HOME"/Library/Preferences/
+ln -fs "$DOTFILES_DIR"/org.m0k.transmission.plist "$HOME"/Library/Preferences/
