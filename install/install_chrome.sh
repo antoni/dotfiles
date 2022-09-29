ef77f3121e98c81a7a56c8093e7cb814646574cb
Antoni <contact@antoni.systems>
2022-09-29 22:58:41 +0200

    WIP Thu 29 Sep 2022 10:58:41 PM CEST

--- install/install_chrome.sh
+++ install/install_chrome.sh
@@ -15,5 +15,5 @@ function install_debian_chrome() {
 	wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 	sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 	sudo apt-get update
-	sudo apt-get install google-chrome-stable
+	sudo apt-get install -y google-chrome-stable
 }
