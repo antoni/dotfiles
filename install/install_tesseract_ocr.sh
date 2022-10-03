10988c5df3b47723336715814c2fee5c9318c795
Antoni <contact@antoni.systems>
2022-10-03 09:11:19 +0200

    WIP Mon 03 Oct 2022 09:11:19 AM CEST

--- install/install_tesseract_ocr.sh
+++ install/install_tesseract_ocr.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 sudo add-apt-repository -y ppa:alex-p/tesseract-ocr5
-sudo apt install -y tesseract-ocr
+sudo apt install -y tesseract-ocr tesseract-ocr-pol
 tesseract --version
