4d8f48745ef25fc1fce7765f6b38ae1d191ee8b2
Antoni <contact@antoni.systems>
2022-11-26 11:49:45 +0100

    WIP Sat 26 Nov 2022 11:49:45 AM CET

--- windows/install_powershell_profile.sh
+++ windows/install_powershell_profile.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-powershell_profile_location=$(wslpath $(powershell.exe -command " Write-Output \$PSHOME"))
+powershell_profile_directory=$(wslpath $(powershell.exe -command " Write-Output \$PSHOME"))
+powershell_profile_location=$(printf "%s\/profile.ps1" $powershell_profile_directory)
 
-cp profile.ps1 $powershell_profile_location
+echo $powershell_profile_directory
+# echo $powershell_profile_location
+
+cat profile.ps1 > "$powershell_profile_directory""/profile.ps1"
