953ba5c68d4712e4e170da1f0f876a7f1a62e92c
Antoni <contact@antoni.systems>
2022-12-02 13:38:10 +0100

    WIP Fri 02 Dec 2022 01:38:10 PM CET

---
+++ windows/windows_symlink.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+mkdir -p /mnt/c/Users/vivob/Documents/PowerShell
+rm -rf /mnt/c/Users/vivob/Documents/PowerShell/profile.ps1
+
+cp profile.ps1 /mnt/c/Users/vivob/Documents/PowerShell
+
+cat /mnt/c/Users/vivob/Documents/PowerShell/profile.ps1
