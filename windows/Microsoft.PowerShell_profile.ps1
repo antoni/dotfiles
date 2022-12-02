8c9e468c1410b078a99b95a582ecb1ce4d22c78e
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2022-12-02 19:54:35 +0100

    WIP Fri 02 Dec 2022 07:54:35 PM CET

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
-# All Users, All Hosts profile
-# On Windows, should be stored - $PSHOME\Profile.ps1
+# Current user – Current host profile
+# On Windows, should be stored in location defined
+# in $profile global PowerShell variable (for Current user – Current host)
 $env:Path+=';C:\Program Files\Vim\vim90'
 
 New-Alias copy_to_clipboard Set-Clipboard
--- windows/install_powershell_profile.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-powershell_profile_directory=$(wslpath $(powershell.exe -command " Write-Output \$PSHOME"))
-powershell_profile_location=$(printf "%s\/profile.ps1" "$powershell_profile_directory")
-
-echo "$powershell_profile_directory"
-# echo $powershell_profile_location
-
-cat profile.ps1 >"$powershell_profile_directory""/profile.ps1"
