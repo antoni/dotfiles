3dd175b616fa3899446ea8e7c11b2795cfbb657c
Antoni <contact@antoni.systems>
2023-01-02 12:33:48 +0100

    WIP Mon 02 Jan 2023 12:33:48 PM CET

--- install/pull_docker_containers.sh
+++ install/pull_docker_containers.sh
@@ -2,7 +2,7 @@
 set -ue
 
 # Running
-# docker run -v ${path_to_host_folder_to_scan}:/path zricethezav/gitleaks:latest [COMMAND] --source="/path" [OPTIONS]
+# docker run -v ${path_to_host_folder_to_scan}:/gitleaks zricethezav/gitleaks:latest detect --no-banner --source="/gitleaks" --verbose
 docker pull zricethezav/gitleaks:latest
 
 docker pull oxsecurity/megalinter:v6
