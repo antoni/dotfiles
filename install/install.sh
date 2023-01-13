80c5d6271c008f025fca5865f1388bf6d2d671fe
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2023-01-13 12:48:03 +0100

    WIP Fri Jan 13 12:48:03 CET 2023

--- install/install.sh
+++ install/install.sh
@@ -295,7 +295,7 @@ function install_tmux_plugin_manager() {
 
 # Make global packages install locally (without sudo)
 function create_npm_global_packages_directory() {
-	NPM_DIR=$HOME/.npm-global
+	NPM_DIR=$HOME/.npm-global_packages
 	mkdir -p "$NPM_DIR"
 	npm config set prefix "$NPM_DIR"
 }
