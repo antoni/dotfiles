63181153cdd21a471c63d9d06b418b387572ddc4
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2023-01-21 18:37:49 +0100

    WIP Sat 21 Jan 2023 06:37:49 PM CET

---
+++ windows/install_cog.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Source: https://github.com/replicate/cog#install
+sudo curl -o /usr/local/bin/cog -L "https://github.com/replicate/cog/releases/latest/download/cog_$(uname -s)_$(uname -m)"
+sudo chmod +x /usr/local/bin/cog
