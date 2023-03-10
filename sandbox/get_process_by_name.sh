21047858a45e87a2047a0de78ec1c276d7674607
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2023-03-10 17:22:13 +0100

    WIP Fri Mar 10 05:22:13 PM CET 2023

--- sandbox/get_process_by_name.sh
+++ sandbox/get_process_by_name.sh
@@ -6,7 +6,7 @@
 function get_process_by_name() {
 	local process_name="$1"
 	process_list=$(
-		powershell.exe -Command Get-Process \| Where \{\$\_.ProcessName -Like \"*"$process_name"*\"\}
+		powershell.exe -NoLogo -Command Get-Process \| Where \{\$\_.ProcessName -Like \"*"$process_name"*\"\}
 		echo unformatted_output
 	)
 	echo "${process_list%unformatted_output}" | head -n -2 | tail -n +4
