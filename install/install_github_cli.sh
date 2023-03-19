dcdaab70e495295c947fb9863ed0c989aa36ac25
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2023-03-19 02:32:46 +0100

    WIP Sun Mar 19 02:32:46 CET 2023

--- install/install_github_cli.sh
+++ install/install_github_cli.sh
@@ -6,4 +6,4 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 	sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
 	sudo apt update &&
-	sudo apt install gh -y
+	sudo apt install --assume-yes gh
