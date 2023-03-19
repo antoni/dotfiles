1456ed239c982a0ecd000027205865eb878c996b
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2023-03-19 02:32:46 +0100

    WIP Sun Mar 19 02:32:46 CET 2023

--- install/install_chrome.sh
+++ install/install_chrome.sh
@@ -16,5 +16,5 @@ function install_debian_chrome() {
 	wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 	sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 	sudo apt-get update
-	sudo apt-get install -y google-chrome-stable
+	sudo apt-get install --assume-yes google-chrome-stable
 }
