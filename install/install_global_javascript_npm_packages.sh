20a719f1b5de01f120f57e95bc4ef048151c4f58
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2023-04-01 14:06:54 +0200

    WIP Sat Apr  1 02:06:54 PM CEST 2023

--- install/install_global_javascript_npm_packages.sh
+++ install/install_global_javascript_npm_packages.sh
@@ -16,7 +16,7 @@ function install_global_javascript_npm_packages() {
 		yo generator-office dts-gen yargs rollup pnpm source-map-explorer \
 		@angular/cli n json5 cordova gltf-pipeline @squoosh/cli depcheck @microsoft/rush \
 		do-not-disturb-cli katex servor degit verdaccio tables gatsby-cli browser-sync \
-		@apidevtools/swagger-cli kill-port-process ngrok @google/clasp
+		@apidevtools/swagger-cli kill-port-process ngrok @google/clasp js-beautify
 
 	# Disable 'npm fund' messages
 	npm config set fund false
