1803ae43b97393e9fd17a2ece4ad49eba2690903
Antoni <contact@antoni.systems>
2023-05-28 00:20:32 +0200

    WIP Thu May 11 18:18:54 +03 2023
    WIP Sun May 14 18:48:48 +03 2023
    
    WIP Mon May 15 01:08:13 PM +03 2023
    
    WIP Mon May 15 02:07:29 PM +03 2023
    
    WIP Mon May 15 02:12:36 PM +03 2023
    
    WIP Mon May 15 02:14:51 PM +03 2023
    
    WIP Fri May 19 10:08:49 CEST 2023
    
    WIP Fri May 19 08:04:33 PM +03 2023
    
    WIP Fri May 19 09:06:04 PM +03 2023
    
    WIP Sun May 21 03:59:13 PM +03 2023
    
    WIP Sun May 21 09:00:27 PM +03 2023
    
    WIP Sun May 21 09:00:47 PM +03 2023
    
    WIP Sun May 21 09:04:39 PM +03 2023
    
    WIP Mon May 22 05:42:29 PM +03 2023
    
    WIP Sat May 27 02:14:16 PM CEST 2023
    
    WIP Sat May 27 08:36:00 PM CEST 2023
    
    WIP Sun May 28 00:14:56 CEST 2023
    
    WIP Sun May 28 12:20:23 AM CEST 2023

---
+++ install/install_latex.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -ue
+
+# Install LaTeX on Ubuntu: https://gist.github.com/rain1024/98dd5e2c6c8c28f9ea9d
+sudo apt-get install -y texlive-latex-base
+sudo apt-get install -y texlive-fonts-recommended
+sudo apt-get install -y texlive-fonts-extra
+sudo apt-get install -y texlive-latex-extra
