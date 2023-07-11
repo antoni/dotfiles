54cd61f6bbb2208268c7854ba64e8244b24a86a3
Antoni <contact@antoni.systems>
2023-07-11 23:12:26 +0200

    WIP Tue Jul 11 11:12:26 PM CEST 2023

---
+++ windows/application-associations/ImportDefaultApplicationAssociations.ps1
@@ -0,0 +1,7 @@
+# https://docs.microsoft.com/en-us/windows-hardware/manufacture/desktop/dism-default-application-association-servicing-command-line-options?view=windows-11
+
+# Export to file
+#Dism.exe /Online /Export-DefaultAppAssociations:C:\ApplicationAssociations.xml
+#Dism.exe /Online /Export-DefaultAppAssociations:ApplicationAssociations.xml
+
+Dism.exe /Online /Import-DefaultAppAssociations:ApplicationAssociations.xml
