50fe4485db4dd25ea4ed79705d8f749b27ff8a2f
Antoni <contact@antoni.systems>
2023-08-06 13:51:15 +0200

    WIP Sun Aug  6 01:51:15 PM CEST 2023

--- windows/apply_wsl_fixes.sh
+++ windows/apply_wsl_fixes.sh
@@ -4,10 +4,9 @@
 # Work in progress
 
 # Fixes:
-# run-detectors: unable to find an interpreter for /mnt/c/Users/vivob/AppData/Local/Programs/Microsoft VS Code/Code.exe
+# run-detectors: unable to find an interpreter for /mnt/c/Users/username/AppData/Local/Programs/Microsoft VS Code/Code.exe
 sudo update-binfmts --disable cli
 
 # Fixes: time inside WSL
 # Get the latest time from your Windows machine’s RTC and set the system time to that
-# TODO: Move to wsl_fixes.sh (create it)
 sudo hwclock --hctosys
