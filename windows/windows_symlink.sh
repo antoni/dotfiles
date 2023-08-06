865cee940941e5715606d33e5d1f332fa0d62084
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2023-08-06 13:51:15 +0200

    WIP Sun Aug  6 01:51:15 PM CEST 2023

--- windows/windows_symlink.sh
+++ windows/windows_symlink.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-powershell_profile_directory="/mnt/c/Users/vivob/Documents/WindowsPowerShell"
+source "$(dirname "$0")/../aliases" || exit 1
+
+powershell_profile_directory="/mnt/c/Users/""$WINDOWS_USERNAME""/Documents/WindowsPowerShell"
 powershell_profile_location=$(printf "%s/Microsoft.PowerShell_profile.ps1" "$powershell_profile_directory")
 
 mkdir -p "$powershell_profile_directory"
