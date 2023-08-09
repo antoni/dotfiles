6e6973aa237b93ee67cd009ab4d7d319f69ce16b
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2023-08-09 16:44:17 +0300

    WIP Wed Aug  9 16:44:17 +03 2023

--- windows/install_golang.sh
+++ windows/install_golang.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
+GOLANG_VERSION="1.16.7"
+
 if command_exists go; then
-	printf "[${colors[Red]}%s${colors[Color_Off]}]\n" \
+	printf "[${colors[Red]}%s${colors[Reset_Color]}]\n" \
 		"golang is already installed"
 	exit
 fi
 
-# TODO: Pass golang version from command line
 function main() {
 	pushd ~/tmp || exit
-	curl -OL https://golang.org/dl/go1.16.7.linux-amd64.tar.gz
+	curl -OL "https://golang.org/dl/go""$GOLANG_VERSION"".linux-amd64.tar.gz"
 
 	echo "Installing Go with the following checksum:"
-	sha256sum go1.16.7.linux-amd64.tar.gz
-	sudo tar -C /usr/local -xvf go1.16.7.linux-amd64.tar.gz
+	sha256sum "go""$GOLANG_VERSION"".linux-amd64.tar.gz"
+	sudo tar -C /usr/local -xvf "go""$GOLANG_VERSION"".linux-amd64.tar.gz"
 
 	popd || exit
 }
