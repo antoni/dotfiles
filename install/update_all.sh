cc85e2186b72dfee9e48180b5ea02ea3311e045a
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2023-08-09 09:12:49 +0300

    WIP Wed Aug  9 09:12:49 +03 2023

--- install/update_all.sh
+++ install/update_all.sh
@@ -16,7 +16,7 @@ function upgrade_apt_packages() {
 function update_all() {
 	pushd ~ &>/dev/null || exit # to correctly update global NPM packages
 
-	if [[ "$unamestr" == 'Darwin' ]]; then
+	if [[ "$UNAME_OUTPUT" == 'Darwin' ]]; then
 		# This sometimes breaks Homebrew taps upgrade
 		# update_xcode
 
