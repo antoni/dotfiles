6d3cf4f83bd6df6440c9e222eaec8ce36862859a
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2023-09-11 14:59:33 +0200

    WIP Mon Sep 11 02:59:33 PM CEST 2023

--- windows/apply_wsl_fixes.sh
+++ windows/apply_wsl_fixes.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
 # Applies various fixes to make WSL work 100% correctly
-# Work in progress
+
+# Fixes: systemd
+# https://github.com/microsoft/WSL/issues/8952#issuecomment-1572193568
+sudo sh -c 'echo :WSLInterop:M::MZ::/init:PF > /usr/lib/binfmt.d/WSLInterop.conf'
+sudo systemctl unmask systemd-binfmt.service
+sudo systemctl restart systemd-binfmt
+sudo systemctl mask systemd-binfmt.service
 
 # Fixes:
-# run-detectors: unable to find an interpreter for /mnt/c/Users/username/AppData/Local/Programs/Microsoft VS Code/Code.exe
+# run-detectors: unable to find an interpreter for
+# /mnt/c/Users/username/AppData/Local/Programs/Microsoft VS Code/Code.exe
 sudo update-binfmts --disable cli
 
 # Fixes: time inside WSL
