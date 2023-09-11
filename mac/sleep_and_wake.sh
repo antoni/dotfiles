ac59652e1f55511cfdc09c08f56b040fb3573d11
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2023-09-11 17:47:44 +0200

    WIP Mon Sep 11 17:47:43 CEST 2023

--- mac/sleep_and_wake.sh
+++ mac/sleep_and_wake.sh
@@ -1,22 +1,32 @@
 #!/usr/bin/env bash
 
-HEADPHONES_BLUETOOTH_DEVICE_ID=cc-98-8b-93-48-5d
+# Connect and connect/disconnect Bluetooth headphones on wake/sleep
+# To find out the ID of the device running:
+# blueutil --connected
+
+# TODO: Move to some constants
+HEADPHONES_BLUETOOTH_DEVICE_ID="88-c9-e8-10-28-75"
 BLUEUTIL_BIN=/usr/local/bin/blueutil
 
 function sleep() {
-	if [ "$($BLUEUTIL_BIN --is-connected $HEADPHONES_BLUETOOTH_DEVICE_ID)" -eq 0 ]; then
-		$BLUEUTIL_BIN --power 0
-	else
-		$BLUEUTIL_BIN --disconnect $HEADPHONES_BLUETOOTH_DEVICE_ID
-	fi
+	# if [ ! "$($BLUEUTIL_BIN --is-connected $HEADPHONES_BLUETOOTH_DEVICE_ID)" -eq 0 ]; then
+	# 	$BLUEUTIL_BIN --disconnect $HEADPHONES_BLUETOOTH_DEVICE_ID \
+	# 		--info $HEADPHONES_BLUETOOTH_DEVICE_ID
+	# fi
+
+	# Just turn Bluetooth off
+	$BLUEUTIL_BIN --power 0
 }
 
 function wake() {
 	if [ "$($BLUEUTIL_BIN --power)" -eq 0 ]; then
 		echo Powering Bluetooth ON
+
 		$BLUEUTIL_BIN --power 1
 		# Wait till Bluetooth is ON
 		/bin/sleep 3
 	fi
-	$BLUEUTIL_BIN --connect $HEADPHONES_BLUETOOTH_DEVICE_ID
+
+	$BLUEUTIL_BIN --connect $HEADPHONES_BLUETOOTH_DEVICE_ID \
+		--info $HEADPHONES_BLUETOOTH_DEVICE_ID
 }
--- mac/wakeup.sh
+++ mac/wakeup.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+# symlinked to "$HOME"/.wakeup (name imposed by sleepwatcher)
+
 source "$HOME"/dotfiles/mac/sleep_and_wake.sh && wake
