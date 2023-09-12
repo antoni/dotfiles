e4c3da8dbd9137f6cec8e3b08873eddf1b02d729
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2023-09-12 13:25:06 +0200

    WIP Tue Sep 12 13:25:06 CEST 2023

---
+++ config.sh
@@ -0,0 +1,9 @@
+# Configuration per machine
+# TODO: Make it per machine
+
+# Source it in other files:
+# source "$HOME"/dotfiles/config.sh
+
+# To find out the ID of the device running:
+# blueutil --connected
+HEADPHONES_BLUETOOTH_DEVICE_ID="88-c9-e8-10-28-75"
