ff88310803ad709ce8b9dc36e3dfe2061b4e7b0a
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2023-10-18 10:57:49 +0200

    WIP Wed Oct 18 10:57:49 CEST 2023

--- tmux_startup.sh
+++ tmux_startup.sh
@@ -2,10 +2,16 @@
 
 source "$HOME"/dotfiles/utils.sh
 
+STARTUP_DIRECTORY="$HOME/design-system"
+
 # Tasks that need to be done once, after Tmux starts.
 # Should be executed from a single pane
 
 log_info "Running one-time terminal startup tasks that require user interaction"
+
+if [ ! -d "$STARTUP_DIRECTORY" ]; then
+	log_warning "$(printf "Startup directory ('%s') does not exist\n" "$STARTUP_DIRECTORY")"
+fi
 
 if [[ -n "$IS_WSL" ]]; then
 	# Fixes:
