8c767b81de2deb6b58d93685d5f9e589880dccf0
Antoni <contact@antoni.systems>
2023-10-24 22:05:47 +0200

    WIP Tue Oct 24 10:05:47 PM CEST 2023

--- windows/install.sh
+++ windows/install.sh
@@ -2,12 +2,15 @@
 
 # shellcheck disable=SC2182
 
+source "$HOME"/dotfiles/config.sh
+
 function copy_scripts_to_main_drive() {
-    local -r windows_scripts_directory="/mnt/c/Users/vivob/scripts"
+    local -r windows_scripts_directory="/mnt/c/scripts"
+
     printf  "Copying scripts to Windows (host) main drive\n"
     rm -rf "$windows_scripts_directory"
 
-    cp -r "$HOME""/scripts/windows" "$windows_scripts_directory"
+    cp -r "$HOME""/scripts" "$windows_scripts_directory"
 }
 
 function main() {
