6cf561f438c0e517cf984b9ca8cc4d23ec41e9d7
Antoni <contact@antoni.systems>
2023-12-14 21:22:52 +0100

    WIP Thu Dec 14 09:22:52 PM CET 2023

--- windows/windows_install.sh
+++ windows/windows_install.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+source "$HOME"/dotfiles/config.sh
+
 # Install Yarn
 curl -o- -L https://yarnpkg.com/install.sh | bash
 
 # Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 
-nvm install --lts
+
+nvm install "$NODE_VERSION"
+nvm alias default "$NODE_VERSION"
 nvm install node
 
 # Disable yarn telemetry
