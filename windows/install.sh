810461c52a28af43bdaa604fac702b3c33ac3bfb
Antoni <contact@antoni.systems>
2024-01-21 13:04:09 +0100

    WIP Sun Jan 21 01:04:09 PM CET 2024

--- windows/install.sh
+++ windows/install.sh
@@ -5,20 +5,20 @@
 source "$HOME"/dotfiles/config.sh
 
 function copy_scripts_to_main_drive() {
-    local -r windows_scripts_directory="/mnt/c/scripts"
+	local -r windows_scripts_directory="/mnt/c/scripts"
 
-    printf  "Copying scripts to Windows (host) main drive\n"
-    rm -rf "$windows_scripts_directory"
+	printf "Copying scripts to Windows (host) main drive\n"
+	rm -rf "$windows_scripts_directory"
 
-    cp -r "$HOME""/scripts" "$windows_scripts_directory"
+	cp -r "$HOME""/scripts" "$windows_scripts_directory"
 }
 
 function main() {
-    copy_scripts_to_main_drive
+	copy_scripts_to_main_drive
 
-    printf "%s\n" "Run the following PowerShell scripts manually to complete installation process:" \
-        "        InstallPowerShell.ps1" \
-        "        PostinstallPowerShell.ps1"
+	printf "%s\n" "Run the following PowerShell scripts manually to complete installation process:" \
+		"        InstallPowerShell.ps1" \
+		"        PostinstallPowerShell.ps1"
 }
 
 main "$@"
