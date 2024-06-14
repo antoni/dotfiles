83335ab272cf6d21c4e1bb911408d74615eb88f6
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2024-06-14 09:53:44 +0200

    WIP Sat  1 Jun 12:16:30 CEST 2024

--- windows/install_swyc_rs.sh
+++ windows/install_swyc_rs.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 function install_swyh_rs() {
-  local -r version_latest="$(gh release list --repo dheijl/swyh-rs --json "isLatest,tagName" | jq --raw-output -c '.[] | select(.isLatest == true).tagName')"
-  local -r install_directory="/mnt/c/Users/$WINDOWS_USERNAME"
-  local -r release_zip_file=swyh-rs-release.zip
+	local -r version_latest="$(gh release list --repo dheijl/swyh-rs --json "isLatest,tagName" | jq --raw-output -c '.[] | select(.isLatest == true).tagName')"
+	local -r install_directory="/mnt/c/Users/$WINDOWS_USERNAME"
+	local -r release_zip_file=swyh-rs-release.zip
 
-  printf "Downloading swyh-rs version %s\n" "$version_latest"
+	printf "Downloading swyh-rs version %s\n" "$version_latest"
 
-  pushd "$install_directory" &>/dev/null || return 1
+	pushd "$install_directory" &>/dev/null || return 1
 
-  wget --quiet --progress=bar:force:noscroll "https://github.com/dheijl/swyh-rs/releases/download/$version_latest/swyh-rs-release.zip"
-  # Unzip with overwrite
-  unzip -qq -o "$release_zip_file"
-  rm "$release_zip_file"
+	wget --quiet --progress=bar:force:noscroll "https://github.com/dheijl/swyh-rs/releases/download/$version_latest/swyh-rs-release.zip"
+	# Unzip with overwrite
+	unzip -qq -o "$release_zip_file"
+	rm "$release_zip_file"
 
-  popd &>/dev/null || return 1
+	popd &>/dev/null || return 1
 }
 install_swyh_rs
