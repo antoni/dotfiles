5de9d5b88ac70be2483c82e6b752333132678f67
Antoni <contact@antoni.systems>
2024-08-11 19:23:56 +0200

    WIP Sun 11 Aug 19:23:56 CEST 2024

--- install/pip_packages.sh
+++ install/pip_packages.sh
@@ -7,5 +7,5 @@ PIP_PACKAGES=(pylint 2to3 pyflakes autopep8
 	matplotlib protobuf conda haruhi-dl google-api-python-client oauth2client progressbar2
 	tdmgr PyQt5 paho-mqtt PyQtWebEngine mvt xmldiff yq poetry beautysh
 	yt-dlp speedtest-cli pipupgrade shodan linode-cli mvt jupyter truffleHog
-	gallery-dl instaloader
+	gallery-dl instaloader git-filter-repo
 )
