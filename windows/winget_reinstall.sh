74da4c3c56da681a7111bebe40409bdac7a8cf91
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2024-08-31 13:05:39 +0200

    WIP Sat 31 Aug 13:05:39 CEST 2024

--- windows/winget_reinstall.sh
+++ windows/winget_reinstall.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
+source "$HOME"/dotfiles/windows/winget_utils.sh
 
 function main() {
 	local -r package_name="$1"
-	# TODO: Use WINGET_ALIAS and WINGET_COMMAND_INSTALL from winget_package.sh (move to utils.sh first)
-	WINGET_ALIAS="winget.exe"
-	${WINGET_ALIAS} uninstall -e --id "$package_name"
-
-	WINGET_COMMAND_INSTALL="$WINGET_ALIAS install \
---no-upgrade \
---accept-source-agreements \
---accept-package-agreements \
---exact \
---id "
-
+	${WINGET_COMMAND_UNINSTALL} "$package_name"
 	${WINGET_COMMAND_INSTALL} "$package_name"
 }
 
