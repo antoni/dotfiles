5e4bfa1cff6d0e4fdadca811a3063de717f6479b
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2024-12-18 01:03:33 +0100

    WIP Wed 18 Dec 01:03:33 CET 2024

--- install/install.sh
+++ install/install.sh
@@ -122,9 +122,9 @@ function main() {
 		#source $DOTFILES_DIR/mac/brew_install.sh
 
 		mac_install_misc
-		HOMEBREW_NO_AUTO_UPDATE=1 brew install "${BREW_PACKAGES_MUST_HAVE[*]}"
+		HOMEBREW_NO_AUTO_UPDATE=1 brew install "${BREW_PACKAGES_MUST_HAVE[@]}"
 
-		HOMEBREW_NO_AUTO_UPDATE=1 brew install --cask "${BREW_CASK_PACKAGES_MAY_HAVE[*]}"
+		HOMEBREW_NO_AUTO_UPDATE=1 brew install --cask "${BREW_CASK_PACKAGES_MAY_HAVE[@]}"
 
 		sudo chown -R "$USER" /Library/Ruby/Gems/
 
