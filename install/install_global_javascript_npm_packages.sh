011ff1e6ba3377650edc75546d846381364e8c02
John Hopkins <contact@antoni.systems>
2026-04-03 12:00:34 +0200

    WIP Fri  3 Apr 12:00:34 CEST 2026

--- install/install_global_javascript_npm_packages.sh
+++ install/install_global_javascript_npm_packages.sh
@@ -19,7 +19,7 @@ function install_global_javascript_npm_packages() {
 		@angular/cli n json5 cordova gltf-pipeline depcheck @microsoft/rush wrangler \
 		do-not-disturb-cli katex servor degit verdaccio tables gatsby-cli browser-sync \
 		@apidevtools/swagger-cli kill-port-process ngrok @google/clasp js-beautify doctoc \
-		ts_dependency_graph syncpack @immich/cli \
+		ts_dependency_graph syncpack @immich/cli markdownlint-cli2 \
 		@google/gemini-cli @openai/codex @anthropic-ai/claude-code
 
 	# Disable 'npm fund' messages
