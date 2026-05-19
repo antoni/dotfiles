3ce04ad9d7271f8a95894686d941bc188aabdbd6
John Hopkins <contact@antoni.systems>
2026-05-19 13:36:38 +0200

    WIP Tue 19 May 13:36:38 CEST 2026

--- install/pipx_packages.sh
+++ install/pipx_packages.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 PIPX_PACKAGES=(
-	pylint pyflakes autopep8 pep8
+	pylint pyflakes autopep8 pycodestyle
 	pgcli mycli awscli speedtest-cli
 	jupyter jupyterlab dl_coursera
 	pirate-get virtualenv haruhi-dl
 	tdmgr poetry beautysh
 	pipupgrade shodan linode-cli mvt
-	truffleHog gallery-dl instaloader
+	trufflehog gallery-dl instaloader
 	git-filter-repo oci-cli csvkit
 	markitdown docling marker-pdf langflow
-	xmldiff yq 2to3 "pg_activity[psycopg]" csvkit
+	xmldiff yq "pg_activity[psycopg]"
 	openai-whisper
 )
 
